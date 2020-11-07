1009c33915b48c8c0bd6e2534f1db897d6ea097e
Dennis O'Keeffe <hello@dennisokeeffe.com>
2020-11-07 21:57:56 +1100

    feat: project completion

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import { IntlProvider } from 'react-intl';
+import { useRouter } from 'next/router';
+import * as locales from '../content/locale';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  const { locale, defaultLocale, pathname } = router;
+  const localeCopy = locales[locale];
+  const messages = localeCopy[pathname];
+
+  return (
+    <IntlProvider
+      locale={locale}
+      defaultLocale={defaultLocale}
+      messages={messages}
+    >
+      <Component {...pageProps} />
+    </IntlProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/alt.js
+++ pages/alt.js
@@ -5,16 +5,10 @@ export default function IndexPage(props) {
   const { formatMessage } = useIntl();
   const f = (id) => formatMessage({ id });
   const router = useRouter();
-  const { locale, locales, defaultLocale } = router;
 
   return (
     <div>
       <h1>{f('hello')}</h1>
-      <p>{f('welcomeMessage')}</p>
-      <br />
-      <p>Current locale: {locale}</p>
-      <p>Default locale: {defaultLocale}</p>
-      <p>Configured locales: {JSON.stringify(locales)}</p>
     </div>
   );
 }
